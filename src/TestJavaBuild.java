f768faa3b5f8da664023b746a78f3904b389d297
abtripat <abtripat@HSC-PF1GWQDZ.allegisgroup.com>
2019-03-11 08:56:32 +0530

    Added initial code

---
+++ src/TestJavaBuild.java
@@ -0,0 +1,8 @@
+
+public class TestJavaBuild {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Jenkins!! This run confirms you have built me successfully !! :)");
+	}
+
+}
